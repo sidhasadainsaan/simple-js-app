13a3905e3e136ab5027b6a7f6eb8518f6adb7be0
sidhasadainsaan <rathod_tejas@outlook.com>
2024-09-02 14:34:57 -0700

    updated scripts.js
    created pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,5 @@
-alert('Hello world');
-
-
-
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
-
-let simpleAddition = 5 + 5;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'bulbasaur', height: '0.7', types: ['grass', 'water']},
+    {name: 'pikachu', height: '0.4', types: 'electric'},
+    {name: 'vulpix', height: '0.6', types: 'fire'}
+];
