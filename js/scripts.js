1a0f567e63d9f633b336dbc875dbbae8c84a67bb
sidhasadainsaan <rathod_tejas@outlook.com>
2024-09-08 15:10:29 -0700

    Updated scripts.js
    Changed the marking to match the formation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonList = [
     {name: 'bulbasaur', height: '0.7', types: ['grass', 'water']},
-    {name: 'pikachu', height: '0.4', types: 'electric'},
-    {name: 'vulpix', height: '0.6', types: 'fire'}
+    {name: 'pikachu', height: '0.4', types: ['electric']},
+    {name: 'vulpix', height: '0.6', types: ['fire']}
 ];
+
