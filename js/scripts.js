c1fc9edf5541e34278ed1478e2fd89b78eb268a9
sidhasadainsaan <rathod_tejas@outlook.com>
2024-09-24 21:53:21 -0700

    Updated scripts.js
    updated with functions such as forEach, add, getAll etc.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,47 @@
-let pokemonList = [
-    {name: 'bulbasaur', height: '0.7', types: ['grass', 'water']},
-    {name: 'pikachu', height: '0.4', types: ['electric']},
-    {name: 'vulpix', height: '0.6', types: ['fire']}
-];
+let pokemonRepository = (function () {
 
-for (let i = 0; i < pokemonList.length; i++) {
-    let pokemon =pokemonList[i];
+    let pokemonList = [
+        { name: 'bulbasaur', height: '0.7', types: ['grass', 'water'] },
+        { name: 'pikachu', height: '0.4', types: ['electric'] },
+        { name: 'vulpix', height: '0.6', types: ['fire'] }
+    ];
+
+
+    const expectedKeys = ['name', 'height', 'types'];
+
+
+    function getAll() {
+        return pokemonList;
+    }
+
+
+    function add(pokemon) {
+        if (typeof pokemon === 'object' &&
+            Object.keys(pokemon).every(key => expectedKeys.includes(key)) &&
+            expectedKeys.every(key => pokemon.hasOwnProperty(key))) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log('Invalid Pokémon data. Please provide an object with only "name", "height", and "types".');
+        }
+    }
+
+
+    function findByName(name) {
+        return pokemonList.filter(function (pokemon) {
+            return pokemon.name.toLowerCase() === name.toLowerCase();
+        });
+    }
+
+
+    return {
+        getAll: getAll,
+        add: add,
+        findByName: findByName
+    };
+})();
+
+
+pokemonRepository.getAll().forEach(function (pokemon) {
     console.log('name:', pokemon.name);
     console.log('height:', pokemon.height);
     console.log('types:', pokemon.types);
@@ -13,4 +49,23 @@ for (let i = 0; i < pokemonList.length; i++) {
     if (pokemon.height <= 0.5) {
         document.write('Wow that is tiny!!');
     }
-}
+});
+
+
+pokemonRepository.add({ name: 'charmander', height: '0.6', types: ['fire'] });
+
+
+let foundPokemon = pokemonRepository.findByName('pikachu');
+console.log('Found Pokémon:', foundPokemon);
+
+
+let missingPokemon = pokemonRepository.findByName('missingno');
+console.log('Search result for non-existent Pokémon:', missingPokemon);
+
+
+console.log(pokemonRepository.getAll());
+
+
+
+
+
