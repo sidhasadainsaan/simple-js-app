35b6ffaaa481f4d082eadceb5ac16b463e0f7f38
sidhasadainsaan <rathod_tejas@outlook.com>
2024-10-03 20:50:52 -0700

    updated all three file
    updated with new commands on scripts.js file, added some css styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,64 @@
 let pokemonRepository = (function () {
 
-    let pokemonList = [
+    let repository = [
         { name: 'bulbasaur', height: '0.7', types: ['grass', 'water'] },
         { name: 'pikachu', height: '0.4', types: ['electric'] },
-        { name: 'vulpix', height: '0.6', types: ['fire'] }
+        { name: 'vulpix', height: '0.6', types: ['fire'] },
     ];
 
-
-    const expectedKeys = ['name', 'height', 'types'];
-
+    function add(pokemon) {
+                    repository.push(pokemon);
+    }
 
     function getAll() {
-        return pokemonList;
+        return repository;
     }
 
-
-    function add(pokemon) {
-        if (typeof pokemon === 'object' &&
-            Object.keys(pokemon).every(key => expectedKeys.includes(key)) &&
-            expectedKeys.every(key => pokemon.hasOwnProperty(key))) {
-            pokemonList.push(pokemon);
-        } else {
-            console.log('Invalid Pokémon data. Please provide an object with only "name", "height", and "types".');
-        }
-    }
-
-
-    function findByName(name) {
-        return pokemonList.filter(function (pokemon) {
-            return pokemon.name.toLowerCase() === name.toLowerCase();
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listpokemon = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        button.addEventListener('click', function () {
+            showDetails(pokemon);
         });
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
     }
 
+    function showDetails(pokemon) {
+        let name = pokemon[0];
+        let  details = pokemon[1];
+        console.log('Name: ${name}');
+        console.log(`Height: ${details.height} meters`);
+		console.log(`Weight: ${details.weight} kg`);
+		console.log(`Type(s): ${details.type.join(', ')}`);
+		if (details.height > 8) {
+			console.log("Wow, that's big!");
+		}
+    }
 
     return {
-        getAll: getAll,
         add: add,
-        findByName: findByName
-    };
-})();
-
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-    console.log('name:', pokemon.name);
-    console.log('height:', pokemon.height);
-    console.log('types:', pokemon.types);
-    document.write('<p>' + pokemon.name + ' | height: ' + pokemon.height + ' | types: ' + pokemon.types + '</p>');
-    if (pokemon.height <= 0.5) {
-        document.write('Wow that is tiny!!');
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails,
     }
-});
+})();
 
 
 pokemonRepository.add({ name: 'charmander', height: '0.6', types: ['fire'] });
 
-
-let foundPokemon = pokemonRepository.findByName('pikachu');
-console.log('Found Pokémon:', foundPokemon);
-
-
-let missingPokemon = pokemonRepository.findByName('missingno');
-console.log('Search result for non-existent Pokémon:', missingPokemon);
-
-
 console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
+
+
+
+
 
 
 
